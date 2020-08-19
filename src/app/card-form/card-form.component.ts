55f946d30086a12a1b262c78055ce133a329af3f
lethalSid <senien.hicks@gmail.com>
2020-08-19 18:51:21 -0400

    added for validation

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -1,21 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { DateFormControl } from '../date-form-control';
 
 @Component({
-  selector: 'app-card-form',
-  templateUrl: './card-form.component.html',
-  styleUrls: ['./card-form.component.css']
+  selector: "app-card-form",
+  templateUrl: "./card-form.component.html",
+  styleUrls: ["./card-form.component.css"],
 })
 export class CardFormComponent implements OnInit {
   cardForm = new FormGroup({
-    name: new FormControl('', [
+    name: new FormControl("", [Validators.required, Validators.minLength(3)]),
+    cardNumber: new FormControl("", [
       Validators.required,
-      Validators.minLength(3)
+      Validators.minLength(16),
+      Validators.maxLength(16),
     ]),
-  })
-  constructor() { }
+    expiration: new DateFormControl("", [
+      Validators.required,
+      Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/),
+      ]),
+    securityCode: new FormControl("", [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(4)
+    ]),
+  });
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() { }
+  onSubmit() {
+    console.log('Form was submitted');
   }
-
 }
